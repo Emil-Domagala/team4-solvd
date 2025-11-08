c0c1fc3cbd0f7694580e8959a0723c46568488ba
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2025-11-08 11:13:11 +0100

    feature game

---
+++ src/features/game/domains/dto/game.dto.ts
@@ -0,0 +1,16 @@
+import { Exclude, Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { GameStatus } from '../game.status';
+
+@Exclude()
+export class GameDto {
+  @Expose() @ApiProperty() id: string;
+  @Expose() @ApiProperty() roomId: string;
+  @Expose() @ApiProperty() teams: string[];
+  @Expose() @ApiProperty() status: GameStatus;
+  @Expose() @ApiProperty() currentRound: number;
+  @Expose() @ApiProperty() currentTurn: number;
+  @Expose() @ApiProperty() scores: Record<string, number>;
+  @Expose() @ApiProperty() createdAt: string;
+  @Expose() @ApiProperty() updatedAt: string;
+}
